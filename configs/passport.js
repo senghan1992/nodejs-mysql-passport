4d1753ccbbc976ac11f23a746c41bc2baf0ef5d9
fitpet <fitpet@fitpetui-MacBookPro.local>
2019-08-27 16:02:39 +0900

    수정1차

--- configs/passport.js
+++ configs/passport.js
@@ -21,6 +21,7 @@ module.exports = (passport) => {
             passReqToCallback: true
         },
         (req, email, password, done) => {
+            console.log("넘어온 password 값 : " + bcrypt.hash(password));
             connection.query('select * from members where loginId = ?', [email], (err, result) => {
                 if (err) return done(err);
                 if (result.length === 0) {
@@ -29,6 +30,7 @@ module.exports = (passport) => {
                 }
                 if (!bcrypt.compareSync(password, result[0].password)) {
                     console.log('패스워드가 일치하지 않습니다');
+                    console.log('DB에서 불러온 password : ' + result[0].password)
                     return done(null, false, { message: 'Incorrect password' });
                 }
                 console.log('로그인 성공');
--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -4,7 +4,11 @@ function login(req, res) {
     passport.authenticate('local-login', (err, user, info) => {
         if(err) return res.json({message : info.message});
         if(!user) return res.json({message : info.message});
-        if(user) return res.json({result : true, message : 'congratulation!'});
+        // 로그인 성공
+        req.logIn(user, function(err){
+            if(err) return res,json(err);
+            return res.json({result : true, message : 'congratulation!'});
+        })
     })(req,res);
     // console.log('index_controller/login method');
     // console.log(req.user);
